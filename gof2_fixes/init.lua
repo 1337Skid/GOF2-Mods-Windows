af60d5ce637e6634043bc19f26df055452bf1f39
j2 <59775826+1337Skid@users.noreply.github.com>
2026-05-14 20:00:42 +0200

    Update init.lua

--- gof2_fixes/init.lua
+++ gof2_fixes/init.lua
@@ -1,5 +1,10 @@
 RegisterEvent("EarlyInit", function()
-	-- NOP the function that is pushing the player when colliding on stations/freighters
+	-- NOP (aka disable) the function that is pushing the player when colliding on stations/freighters
 	memoryutils:PatchByte(0x4AF717, 0x90)
 	memoryutils:PatchByte(0x4AF718, 0x90)
+    -- Disable boost
+	memoryutils:PatchByte(0x494D31 + 0, 0xD9) -- fldz
+    memoryutils:PatchByte(0x494D31 + 1, 0xEE) -- fldz
+	memoryutils:PatchByte(0x494D31 + 2, 0x90) -- mostly padding
+    memoryutils:PatchByte(0x494D31 + 3, 0x90)
 end)
