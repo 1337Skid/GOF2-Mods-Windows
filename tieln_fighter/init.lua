53bb887cc1efeae1cd5ab69aeb00a27d84af0f74
j2 <59775826+1337Skid@users.noreply.github.com>
2026-05-22 22:10:42 +0200

    Update init.lua

--- tieln_fighter/init.lua
+++ tieln_fighter/init.lua
@@ -18,15 +18,15 @@ HookFunction("Globals::init", function(ctx)
 			equipmentslots = 8,
 			handling = 117,
 			baseprice = 246568,
-	        	primary_positions = { {-33, 113, 120}, {35, 113, 120} },
-	        	secondary_positions = { {0, 133, 106} },
-	        	turret_positions = { {0, 0, 0} },
+	        primary_positions = { {-33, 113, 120}, {35, 113, 120} },
+	        secondary_positions = { {0, 133, 106} },
+	        turret_positions = { {0, 0, 0} },
 			engines = {
-	    			{ pos = {0, 199, -336}, intensity = {0.4, 0.4, 0.4} }
-				}
+	    		{ pos = {0, 199, -336}, intensity = {0.4, 0.4, 0.4} }
+			}
     	}
     	shipid = ctx:CreateShip("TIE/LN starfighter", "The first TIE fighter was found floating in space by terran explorers in the Beidan system shortly after the appearance of the Void. Postponing the construction of Kothar station, the terrans set out to replicate the ship. It's origins are still unknown.", shipinfo, diffuse, normal, material, lod, lod, lod)
-	print("[TIE/LN Fighter] Injected ship successfully id: " .. shipid .. ", original model by Kroete")
+		print("[TIE/LN Fighter] Injected ship successfully id: " .. shipid .. ", original model by Kroete")
 end)
 
 RegisterEvent("OnStationChanged", function(id)
